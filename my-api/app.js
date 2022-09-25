952f5591993f5981828b98cb05769b70af66a801
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-26 02:27:39 +0600

    Request limiter done

--- my-api/app.js
+++ my-api/app.js
@@ -1,18 +1,46 @@
 const express = require('express');
+//Router Import
+const router = require("./src/routes/api");
 const app = new express();
 
-//Router Import
-const router=require("./src/routes/api");
+//Security Middleware
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+const cors = require('cors')
 
-app.use("/api/v1",router)
+
+//Security Middleware Implement
+
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp());
+
+// Request Rate Limiting
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 100,// 15 Minutes
+    max: 100 // Limit each IP to 100 requests per minutes
+
+})
+
+app.use(limiter)
+
+//undefined Route
+app.use("/api/v1", router)
+
+app.use('*', (req, res) => {
+    res.status(404).json({status: "Failed", data: "Not Found"});
+})
 
 //baseurl=api/v1/
 
-//undefined Route
-
-app.use('*',(req,res)=>{
-    res.status(404).json({status:"Failed",data:"Not Found"});
-})
-
 //export
-module.exports=app;
+module.exports = app;
+
+
+
