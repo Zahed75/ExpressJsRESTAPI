17e72240aece4b92c73e5602d9524611c1c69f22
Zahed75 <Zahedhasan.CS@gmail.com>
2022-09-27 21:49:29 +0600

    done everything

--- my-api/src/models/EmployeeModel.js
+++ my-api/src/models/EmployeeModel.js
@@ -3,7 +3,12 @@ const mongoose=require('mongoose')
 const DataSchema=mongoose.Schema({
     Name:{type:String,unique:true},
     Role:{type:String,required:true},
-    Salary:{type:Number, min:6, max:12},
+    Salary:{type:Number,
+    //    custom error message
+        min:[6,"Minimum 6 Max 12 but Supplied value is ={VALUE}"],
+        max:[12,"Minimum 6 Max 12 but Supplied value is ={VALUE}"]
+
+    },
     Adult:{type:Boolean},
     Comments:[],
     Address:{type:String,default:"Dhaka"},
--- my-api/src/models/TeacherModel.js
+++ my-api/src/models/TeacherModel.js
@@ -1,11 +1,40 @@
-const mongoose=require('mongoose')
-const DataSchema=mongoose.Schema({
-    Name:String,
-    Faculty:String,
-    EmId:String,
-    Salary:String,
+const mongoose = require('mongoose')
+const DataSchema = mongoose.Schema({
+    Name: {type: String, unique: true},
+    Faculty: {type: String},
+    EmId: {type: String, unique: true},
+    Salary: {type: Number},
+    //custom validation message
+    Mobile: {
+        type: String,
+        validate: {
+            validator: function (value) {
+                if(value.length=== 11) {
+                    return true
+                }
+                else {
+                    return false
+                }
+
+
+            },
+            message:" Phone Number Must be 11 Digit Required!"
+        }
+
+    },
+    //regex validations
+    CompanyNumber:{
+        type:String,
+        validate: {
+            validator:function (value) {
+                return /(^(\+88|0088)?(01){1}[3456789]{1}(\d){8})$/.test(value)
+            },
+            message:"Invalid BD Number Please Check your number"
+        }
+    }
+
 })
 
-const TeacherModel= mongoose.model("teachers",DataSchema)
+const TeacherModel = mongoose.model("teachers", DataSchema)
 
-module.exports=TeacherModel;
+module.exports = TeacherModel;
